6c9bca9d8680878107703ec58ee7b12906ac98ed
Eric Esteban <ericeru@ext.stradivarius.com>
2026-06-25 18:01:17 +0200

    feat: integrate cucumber html reporter and gh-pages

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,3 +1,6 @@
+const report = require('multiple-cucumber-html-reporter');
+const fs = require('fs-extra');
+
 exports.config = {
     runner: 'local',
     specs: [
@@ -18,9 +21,25 @@ exports.config = {
     connectionRetryTimeout: 120000,
     connectionRetryCount: 3,
     framework: 'cucumber',
-    reporters: ['spec'],
+    reporters: ['spec', ['cucumberjs-json', {
+        jsonFolder: '.tmp/json/',
+        language: 'en',
+    }]],
     cucumberOpts: {
         require: ['./test/step-definitions/**/*.js'],
         timeout: 120000 // Subido a 2 minutos para el flujo de TV
+    },
+    onPrepare: () => {
+        fs.removeSync('.tmp/');
+    },
+    onComplete: () => {
+        report.generate({
+            jsonDir: '.tmp/json/',
+            reportPath: './reports/html/',
+            openReportInBrowser: false,
+            pageTitle: 'Smart TV Automation PoC',
+            reportName: 'YouTube App TV Tests',
+            displayDuration: true
+        });
     }
 }
